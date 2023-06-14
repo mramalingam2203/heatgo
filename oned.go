cfd762f6620a8cd9a08de73b596c62a5fc85f32b
kickback-consulting <consulting@kickback.space>
2023-06-14 17:33:43 +0530

    frankel code

--- oned.go
+++ oned.go
@@ -66,6 +66,31 @@ func exact(nodes int32, Diff float64, output_time float64, delta_x float64, leng
 
 }
 
+func DuFort_Frankel_Explicit_Scheme(delta_x float64, delta_t float64, output_time float64, nodes int32, r float64, T_sur float64, T_init float64) {
+
+	numerical := make([][]float64, 0.0)
+
+	for n := 0; n <= int(output_time/delta_t); n++ {
+		numerical[0][n] = T_sur       // BC at 0 and all n (node #0)
+		numerical[nodes-1][n] = T_sur // BC at 31 and all n (node #last_node)
+	}
+
+	for i := 1; i < int(nodes-1); i++ { // Initial Conditions #1
+		numerical[i][0] = T_init // IC at n = 0, for all i, except node node i = 0 and i = nodes
+	}
+
+	for i := 1; i < int(nodes-1); i++ { // Initial Conditions #2
+		numerical[i][1] = r*numerical[i-1][0] + (1.0-(2.0*r))*numerical[i][0] + r*numerical[i+1][0] // IC at n = 1, for all i, except node node i = 0 and i = nodes
+	}
+
+	for n := 1; n < int(output_time/delta_t); n++ {
+		for i := 1; i < int(nodes-1); i++ {
+			numerical[i][n+1] = ((2.0*r)/(1.0+2.0*r))*numerical[i-1][n] + ((2.0*r)/(1.0+2.0*r))*numerical[i+1][n] + ((1.0-2.0*r)/(1.0+2.0*r))*numerical[i][n-1]
+		}
+	}
+
+}
+
 func explicit() {
 
 }
@@ -73,6 +98,7 @@ func explicit() {
 func implicit() {
 
 }
+
 func cranknic() {
 
 }
