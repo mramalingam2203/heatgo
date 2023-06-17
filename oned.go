625a4953b8c55d3741bc679dfbcb6f52d12aaa0a
kickback-consulting <consulting@kickback.space>
2023-06-17 10:20:36 +0530

    split files

--- oned.go
+++ oned.go
@@ -2,22 +2,7 @@ package main
 
 import (
 	"fmt"
-	_ "image"
-	_ "image/color"
-	_ "image/draw"
-	_ "image/png"
-	_ "log"
 	"math"
-	_ "math/rand"
-	_ "os"
-	_ "sort"
-	_ "time"
-
-	_ "github.com/ajstarks/svgo"
-	_ "github.com/vdobler/chart"
-	_ "github.com/vdobler/chart/imgg"
-	_ "github.com/vdobler/chart/svgg"
-	_ "github.com/vdobler/chart/txtg"
 )
 
 var pi float64 = 4 * math.Atan(1.0)
@@ -196,48 +181,5 @@ func TDMA_Solver(lower_diag []float64, main_diag []float64, upper_diag []float64
 	for i := nodes; i > 0; i-- {
 		b[i] -= upper_diag[i] * b[i+1]
 	}
-
-}
-
-func main() {
-	// Variables meant for 1-D calculations only
-
-	/*
-		xmin - minimum value of x
-		xmax - maximum value of x
-		dX - distance inrements
-		dt - time increment
-		maxtime - total time extent
-		r - Courant Number = alpha*dt/dx*dx
-		alpha - thermal diffusivity = rho*cp/K
-		Tl - Non-dimensional Temperature at left end
-		Tr - non-dimensional temperature at right end
-		nx - total number of nodes in X-direction
-		nt - total number of time steps
-	*/
-	var xmin, xmax, dX, dt, maxtime, rvalue, alpha, Tl, Tr float64
-	var nX, nt int
-
-	alpha = 0.0005
-	xmin = 0
-	xmax = 0.2
-	nX = 10
-	Tl = 300.0
-	Tr = 300.0
-	dX = (xmax - xmin) / float64(nX-1)
-	dt = 4.0812e-2
-	maxtime = 10
-	nt = int(maxtime / dt)
-
-	dX = float64(xmax-xmin) / float64(nX-1)
-	dt = float64(maxtime) / float64(nt)
-	fmt.Println(dX, dt)
-
-	rvalue = alpha * dt / dX * dX
-	fmt.Println(rvalue)
-
-	//explicit_matlab(nt, nX, dt, Tl, Tr, rvalue)
-	//exact(nX, float64(alpha), float64(maxtime), float64(dX), float64(1.0), float64(Tr), float64(Tl))
-	DuFort_Frankel_Explicit_Scheme(nt, nX, dX, dt, maxtime, rvalue, Tr, Tl)
 
 }
