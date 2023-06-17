57760e43abb92dd0e480489dce4c388a7664c8d8
kickback-consulting <consulting@kickback.space>
2023-06-17 09:21:04 +0530

    checked DF

--- oned.go
+++ oned.go
@@ -2,36 +2,33 @@ package main
 
 import (
 	"fmt"
+	_ "image"
+	_ "image/color"
+	_ "image/draw"
+	_ "image/png"
+	_ "log"
 	"math"
+	_ "math/rand"
+	_ "os"
+	_ "sort"
+	_ "time"
 
-
-	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
-	"log"
-	"math"
-	"math/rand"
-	"os"
-	"sort"
-	"time"
-
-	"github.com/ajstarks/svgo"
-	"github.com/vdobler/chart"
-	"github.com/vdobler/chart/imgg"
-	"github.com/vdobler/chart/svgg"
-	"github.com/vdobler/chart/txtg"
+	_ "github.com/ajstarks/svgo"
+	_ "github.com/vdobler/chart"
+	_ "github.com/vdobler/chart/imgg"
+	_ "github.com/vdobler/chart/svgg"
+	_ "github.com/vdobler/chart/txtg"
 )
 
 var pi float64 = 4 * math.Atan(1.0)
 
-func explicit_matlab(ntime int32, nx int32, dt float32, T_left float32, T_right float32, r float32) {
-	t := make([]float32, ntime)
-	T := make([]float32, nx)
-	T0 := make([]float32, nx)
+func explicit_matlab(ntime int32, nx int32, dt float64, T_left float64, T_right float64, r float64) {
+	t := make([]float64, ntime)
+	T := make([]float64, nx)
+	T0 := make([]float64, nx)
 
 	for i := 0; i < int(ntime); i++ {
-		t[i] = float32(i) * dt
+		t[i] = float64(i) * dt
 	}
 
 	for j := 0; j < int(nx); j++ {
@@ -83,11 +80,14 @@ func exact(nodes int32, Diff float64, output_time float64, delta_x float64, leng
 
 }
 
-func DuFort_Frankel_Explicit_Scheme(delta_x float64, delta_t float64, output_time float64, nodes int32, r float64, T_sur float64, T_init float64) {
+func DuFort_Frankel_Explicit_Scheme(nt int, nodes int, delta_x float64, delta_t float64, output_time float64, r float64, T_sur float64, T_init float64) {
 
-	numerical := make([][]float64, 0.0)
-
-	for n := 0; n <= int(output_time/delta_t); n++ {
+	numerical := make([][]float64, nt)
+	for i := 0; i < nt; i++ {
+		numerical[i] = make([]float64, nodes)
+	}
+	fmt.Println(len(numerical[0]))
+	for n := 0; n < nodes; n++ {
 		numerical[0][n] = T_sur       // BC at 0 and all n (node #0)
 		numerical[nodes-1][n] = T_sur // BC at 31 and all n (node #last_node)
 	}
@@ -100,11 +100,14 @@ func DuFort_Frankel_Explicit_Scheme(delta_x float64, delta_t float64, output_tim
 		numerical[i][1] = r*numerical[i-1][0] + (1.0-(2.0*r))*numerical[i][0] + r*numerical[i+1][0] // IC at n = 1, for all i, except node node i = 0 and i = nodes
 	}
 
-	for n := 1; n < int(output_time/delta_t); n++ {
-		for i := 1; i < int(nodes-1); i++ {
+	for i := 1; i < nt-1; i++ {
+		for n := 1; n < nodes-1; n++ {
 			numerical[i][n+1] = ((2.0*r)/(1.0+2.0*r))*numerical[i-1][n] + ((2.0*r)/(1.0+2.0*r))*numerical[i+1][n] + ((1.0-2.0*r)/(1.0+2.0*r))*numerical[i][n-1]
 		}
 	}
+
+	fmt.Println(numerical)
+
 }
 
 func Richardson_Explicit_Scheme(delta_x float64, delta_t float64, output_time float64, nodes int32, r float64, T_sur float64, T_init float64) {
@@ -197,8 +200,8 @@ func TDMA_Solver(lower_diag []float64, main_diag []float64, upper_diag []float64
 }
 
 func main() {
-	var xmin, xmax, dX, dt, maxtime, rvalue, alpha, Tl, Tr float32
-	var nX, nt int32
+	var xmin, xmax, dX, dt, maxtime, rvalue, alpha, Tl, Tr float64
+	var nX, nt int
 
 	alpha = 0.05
 	xmin = 0
@@ -206,18 +209,20 @@ func main() {
 	nX = 100
 	Tl = 150.0
 	Tr = 300.0
-	dX = (xmax - xmin) / float32(nX-1)
+	dX = (xmax - xmin) / float64(nX-1)
 	dt = 4.0812e-2
 	maxtime = 1000
-	nt = int32(maxtime / dt)
+	nt = int(maxtime / dt)
 
-	dX = float32(xmax-xmin) / float32(nX-1)
-	dt = float32(maxtime) / float32(nt)
+	dX = float64(xmax-xmin) / float64(nX-1)
+	dt = float64(maxtime) / float64(nt)
 	fmt.Println(dX, dt)
 
 	rvalue = alpha * dt / dX * dX
 	fmt.Println(rvalue)
 
-	explicit_matlab(nt, nX, dt, Tl, Tr, rvalue)
+	//explicit_matlab(nt, nX, dt, Tl, Tr, rvalue)
+	//exact(nX, float64(alpha), float64(maxtime), float64(dX), float64(1.0), float64(Tr), float64(Tl))
+	DuFort_Frankel_Explicit_Scheme(nt, nX, dX, dt, maxtime, rvalue, Tr, Tl)
 
 }
